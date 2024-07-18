a9d856ecac85343e0a1ddda56965259e82536563
benjilabanane <fassi.benjamin@gmail.com>
2024-07-18 16:10:16 +0000

    first commit

---
+++ SPARK_MODULE/API/API.py
@@ -0,0 +1,32 @@
+from kafka import KafkaProducer
+import time,json,requests
+from datetime import datetime
+import  configuration as c
+
+url = c.stock_data_from_api
+
+params = {
+    "adjusted": "true",
+    "apiKey": "4L8BLqY1mDFQIZnC0OzistwsUfrVHNKT"
+}
+
+response = requests.get(url=url,params=params)
+
+def fetch_and_produce_stock_data():
+    if response.status_code == 200:
+
+        parsed_data = json.loads(response.text) 
+
+        for row in parsed_data['results']:
+                print(row)
+                producer = KafkaProducer(bootstrap_servers="course-kafka:9092")
+                producer.send(topic="stock_data", value=json.dumps(row).encode('utf-8'))
+                time.sleep(1)
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+
+if __name__ == "__main__":
+    fetch_and_produce_stock_data()
+
+
+  
