e12c9cdee8034b4246a993d79f62e734cf46351c
benjilabanane <fassi.benjamin@gmail.com>
2024-07-20 13:44:39 +0000

    API - stock_data --> stock-metadata --> stock-metadata-to s3

---
+++ SPARK_MODULE/configuration.py
@@ -0,0 +1,11 @@
+
+######################################################################################
+####################################URL_DATA##########################################
+######################################################################################
+
+stock_data_from_api = "https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09"
+
+base_url_stock_meta_data = "https://api.polygon.io/v3/reference/tickers?active=true&limit=100&market=stocks&apiKey="
+
+api_keys = "4L8BLqY1mDFQIZnC0OzistwsUfrVHNKT"
+
