d10855c549c55a58a9617e2fbeb91b444474a9ac
benjilabanane <fassi.benjamin@gmail.com>
2024-07-21 12:58:53 +0000

    w

--- SPARK_MODULE/API/api_get_stock_data.py
+++ SPARK_MODULE/API/api_get_stock_data.py
@@ -1,42 +1,44 @@
 from kafka import KafkaProducer
-import time,json,requests
+import time
+import json
+import requests
 import configuration as c
-from datetime import datetime,timedelta,date
+from datetime import datetime, timedelta
 
 base_url = c.stock_data_from_api
-end_date = datetime.now()  - timedelta(days=1) # Current date
+end_date = datetime.now() - timedelta(days=1)  # Current date
 start_date = end_date - timedelta(days=5)  # 5 days ago
 
 params = {
     "adjusted": "true",
-    "apiKey": "4L8BLqY1mDFQIZnC0OzistwsUfrVHNKT"
+    "apiKey": c.api_key  # Assuming you have moved the API key to the configuration file
 }
 
-
-def fetch_and_produce_stock_data(date):
-
+def fetch_and_produce_stock_data(producer, date):
     date_string = date.strftime('%Y-%m-%d')
     url = f"{base_url}{date_string}"
-    response = requests.get(url=url,params=params)
+    
+    try:
+        response = requests.get(url=url, params=params)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        parsed_data = response.json()  # Directly get the JSON data
 
-    if response.status_code == 200:
-
-        parsed_data = json.loads(response.text) 
-        
-
-        for row in parsed_data['results']: 
-                row['date_time'] = date_string
-                producer = KafkaProducer(bootstrap_servers="course-kafka:9092")
-                producer.send(topic="stock_data_test", value=json.dumps(row).encode('utf-8'))
-                print(row)
-    else:
-        print(f"Failed to retrieve data: {response.status_code}")
+        for row in parsed_data.get('results', []):
+            row['date_time'] = date_string
+            producer.send(topic="stock_data_test", value=json.dumps(row).encode('utf-8'))
+            print(row)
+    except requests.RequestException as e:
+        print(f"Failed to retrieve data for {date_string}: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 if __name__ == "__main__":
+    producer = KafkaProducer(bootstrap_servers="course-kafka:9092")
     current_date = start_date
+
     while current_date <= end_date:
-        fetch_and_produce_stock_data(current_date)
+        fetch_and_produce_stock_data(producer, current_date)
         current_date += timedelta(days=1)
 
-
-  
+    producer.flush() 
+    producer.close()
