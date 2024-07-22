e4732ee8863d2fbea9765474b96021dd86d3a05b
benjilabanane <fassi.benjamin@gmail.com>
2024-07-22 17:11:10 +0000

    c

--- SPARK_MODULE/postgres_app/postgres_test.py
+++ SPARK_MODULE/postgres_app/postgres_test.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 
 def create_spark_session(app_name: str) -> SparkSession:
     return SparkSession \
@@ -8,13 +8,13 @@ def create_spark_session(app_name: str) -> SparkSession:
         .config("spark.jars", "/opt/spark/jars/postgresql-42.7.3.jar") \
         .getOrCreate()
 
-def read_parquet_from_s3(spark: SparkSession, path: str):
+def read_parquet_from_s3(spark: SparkSession, path: str) -> DataFrame:
 
     return spark.read \
         .format("parquet") \
         .load(path)
 
-def write_stock_transformed_data(df):
+def write_stock_transformed_data(df:DataFrame):
 
     jdbc_url = "jdbc:postgresql://postgres:5432/airflow"
     connection_properties = {
@@ -24,7 +24,7 @@ def write_stock_transformed_data(df):
     }
     
     df.write \
-        .jdbc(url=jdbc_url, table="market_data", mode="append", properties=connection_properties)
+        .jdbc(url=jdbc_url, table="market_data", mode="overwrite", properties=connection_properties)
 
 if __name__ == '__main__':
     app_name = "S3ParquetProcessing"
