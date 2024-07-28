eb9beb9f270ed9ea4916d1caacc51e16159ac69e
benjilabanane/bin/python3 <fassi.benjamin@gmail.com>
2024-07-28 18:25:22 +0000

    cc

--- SPARK_MODULE/configuration.py
+++ SPARK_MODULE/configuration.py
@@ -3,9 +3,7 @@
 ######################################################################################
 
 stock_data_from_api = "https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/"
-
 base_url_stock_meta_data = "https://api.polygon.io/v3/reference/tickers?active=true&limit=100&market=stocks&apiKey="
-
 api_key = "4L8BLqY1mDFQIZnC0OzistwsUfrVHNKT"
 
 ######################################################################################
