4937f22cd428ee8d7ebeab2a58450fd5e022cd5c
benjilabanane <fassi.benjamin@gmail.com>
2024-08-06 07:53:16 +0000

    add option for investgate multiple stocks - the stock to investigate are inside the configuration file

--- SPARK_MODULE/API/api_get_stock_news.py
+++ SPARK_MODULE/API/api_get_stock_news.py
@@ -5,28 +5,28 @@ import time
 import json
 from typing import Optional
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import udf, col, explode, from_json, arrays_zip,lit
+from pyspark.sql.functions import col, explode, from_json, arrays_zip,lit
 from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 import SPARK_MODULE.configuration  as c
 from pyspark.sql import functions as F
 from datetime import datetime, timedelta
 
 # Configuration
-news_from_date =datetime.now() - timedelta(days=90) 
+NEWS_FROM_DATE =datetime.now() - timedelta(days=100) 
 
-BASE_URL = f"{c.stock_data_news}&apiKey={c.api_key}&published_utc.gt={news_from_date.strftime('%Y-%m-%dT%H:%M:%SZ')}"
+BASE_URL = f"{c.stock_data_news}&apiKey={c.api_key}&published_utc.gt={NEWS_FROM_DATE.strftime('%Y-%m-%dT%H:%M:%SZ')}&ticker="
+STOCKS_FOR_INVESTIGATION = c.stocks_for_investigation.split(',')
 MAX_REQUESTS_PER_MINUTE = 5
+result_data = []
 
 params = {
     "adjusted": "true",
     "apiKey": c.api_key
 }
 
-def fetch_data(url: str) -> str:
-    request_count = 0
-    result_data = []
-
+def fetch_data(url: str, request_count:int) -> str:   
     while url:
+        print(request_count)
         if request_count >= MAX_REQUESTS_PER_MINUTE:
             print("rated limit wait 60 second...")
             time.sleep(62)
@@ -50,7 +50,8 @@ def fetch_data(url: str) -> str:
         url = next_url
         request_count += 1
 
-    return json.dumps(result_data)
+    return request_count 
+
 
 def get_spark_session() -> SparkSession:
     return SparkSession.builder \
@@ -98,27 +99,34 @@ def transform_data(df: DataFrame) -> DataFrame:
         F.date_format(F.to_date(F.col('published_utc'), 'yyyy-MM-dd\'T\'HH:mm:ss\'Z\''), 'yyyy-MM-dd').alias('publication_date'),
         col('insights.ticker').alias('ticker'),
         col('insights.sentiment')
-    )
+    ).dropDuplicates()
 
     df_exploded_ticker = df_ticker.withColumn("data_exploded", arrays_zip("ticker", "sentiment"))
     df_ticker_2 = df_exploded_ticker.withColumn("data_exploded", explode("data_exploded"))
 
     return df_ticker_2.select(
+        col("data_exploded.ticker").alias("ticker"),
         "article_title",
         "publisher",
         "author",
         "publication_date",
-        col("data_exploded.ticker").alias("ticker"),
-        col("data_exploded.sentiment").alias("sentiment")
-    ).filter(col('ticker') == 'ORCL')
+        col("data_exploded.sentiment").alias("sentiment"),
+        col("ticker").alias("related_ticker")
+    ).filter(col('ticker').isin(STOCKS_FOR_INVESTIGATION))
 
 def write_to_postgres(df: DataFrame,connection_properties: dict, jdbc_url: str) -> None:
     df.write \
         .jdbc(url=jdbc_url, table="articles", mode="overwrite", properties=connection_properties)
     
 if __name__ == '__main__':
+    
+    request_count = 0
 
-    json_data = fetch_data(BASE_URL)
+    for stock in STOCKS_FOR_INVESTIGATION:
+        #update the request_count in order to not fall because of limitation of the API and fill up the result_data
+        request_count = fetch_data(BASE_URL + stock, request_count)
+        
+    json_data = json.dumps(result_data)
 
     spark = get_spark_session()
     df = spark.createDataFrame([(BASE_URL,)], ["url"])
